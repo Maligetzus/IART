1ec43166410e80b7e924516f2e10b02922f4b683
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2020-03-31 19:24:37 +0100

    Progress

--- analysis.py
+++ analysis.py
@@ -1,25 +1,28 @@
 from neutron import Neutron
-from neutron_util import BoardTypes, Player, get_next_move, get_time_miliseconds
+from neutron_util import BoardTypes, Player, get_next_move, get_time_miliseconds, PlayerTypes
 
 
 def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
     game = Neutron(board_type=board_type)
 
+    game.player_type['White'] = PlayerTypes.CpuL2
+    game.player_type['Black'] = PlayerTypes.CpuL1
 
     game.start()
 
     n_white_plays = 0
     n_black_plays = 0
-    while not game.has_finished():
+    while not game.has_finished()[0]:
         if game.curr_player == Player.White:
             calculation_start_time = get_time_miliseconds()
             neutron_move, pawn_coords, pawn_move = get_next_move(game, heuristic, depth)
             calculation_end_time = get_time_miliseconds()
+
             print("White bot took ", (calculation_end_time - calculation_start_time)/1000, "s to calculate his move")
             if game.turn == Neutron:  # 1st play doesnt move the neutron
                 game.move_piece(game.neutron_position[0], game.neutron_position[1], neutron_move)
                 n_white_plays += 1
-                if game.has_finished():
+                if game.has_finished()[0]:
                     break
 
             game.move_piece(pawn_coords[0], pawn_coords[1], pawn_move)
@@ -32,10 +35,11 @@ def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
             print("Black bot took ", (calculation_end_time - calculation_start_time)/1000, "s to calculate his move")
             game.move_piece(game.neutron_position[0], game.neutron_position[1], neutron_move)
             n_black_plays += 1
-            if game.has_finished():
+            if game.has_finished()[0]:
                 break
             game.move_piece(pawn_coords[0], pawn_coords[1], pawn_move)
-            n_black_plays
+            n_black_plays += 1
+
     print("Game ended")
 
 
