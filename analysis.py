9ea02ccf385272551f543b8197009f6ed1e98972
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2020-03-31 19:46:15 +0100

    Analysis working I think

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 from neutron import Neutron
-from neutron_util import BoardTypes, Player, get_next_move, get_time_miliseconds, PlayerTypes
+from neutron_util import BoardTypes, Player, get_next_move, get_time_miliseconds, PlayerTypes, Turn
 
 
 def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
@@ -12,14 +12,16 @@ def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
 
     n_white_plays = 0
     n_black_plays = 0
-    while not game.has_finished()[0]:
+    game_ended = game.has_finished()[0]
+    while not game_ended:
         if game.curr_player == Player.White:
             calculation_start_time = get_time_miliseconds()
             neutron_move, pawn_coords, pawn_move = get_next_move(game, heuristic, depth)
             calculation_end_time = get_time_miliseconds()
 
-            print("White bot took ", (calculation_end_time - calculation_start_time)/1000, "s to calculate his move")
-            if game.turn == Neutron:  # 1st play doesnt move the neutron
+            print("White bot took ", (calculation_end_time - calculation_start_time) / 1000, "s to calculate his move")
+
+            if game.turn == Turn.Neutron:
                 game.move_piece(game.neutron_position[0], game.neutron_position[1], neutron_move)
                 n_white_plays += 1
                 if game.has_finished()[0]:
@@ -32,23 +34,24 @@ def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
             calculation_start_time = get_time_miliseconds()
             neutron_move, pawn_coords, pawn_move = get_next_move(game, heuristic_op, depth_op)
             calculation_end_time = get_time_miliseconds()
+
             print("Black bot took ", (calculation_end_time - calculation_start_time)/1000, "s to calculate his move")
-            game.move_piece(game.neutron_position[0], game.neutron_position[1], neutron_move)
-            n_black_plays += 1
-            if game.has_finished()[0]:
-                break
+
+            if game.turn == Turn.Neutron:
+                game.move_piece(game.neutron_position[0], game.neutron_position[1], neutron_move)
+                n_black_plays += 1
+                if game.has_finished()[0]:
+                    break
+
             game.move_piece(pawn_coords[0], pawn_coords[1], pawn_move)
             n_black_plays += 1
 
+        game_ended = game.has_finished()[0]
+
     print("Game ended")
+    print("Black bot played:", n_black_plays, "times.")
+    print("White bot played:", n_white_plays, "times.")
 
-
-# for board_type in BoardTypes:
-#     for depth in range(1, 3):
-#         for heuristic in range(1, 3):
-#
-#             for depth_op in range(1, 3):
-#                 for heuristic_op in range(1, 3):
 
 if __name__ == '__main__':
     play_game(BoardTypes.Board_5X5, 1, 1, 3, 3)
