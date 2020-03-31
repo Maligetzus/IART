5f78c96bdb0b4e0c6dc7270c21e8ac7b86a7bf66
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2020-03-31 22:39:19 +0100

    More changes to analysis

--- analysis.py
+++ analysis.py
@@ -1,12 +1,13 @@
 from neutron import Neutron
 from neutron_util import BoardTypes, Player, get_next_move, get_time_miliseconds, PlayerTypes, Turn
+import pygame
 
 
 def play_game(board_type, depth, heuristic, depth_op, heuristic_op):
     game = Neutron(board_type=board_type)
 
-    game.player_type['White'] = PlayerTypes.CpuL2
-    game.player_type['Black'] = PlayerTypes.CpuL1
+    game.player_type['White'] = PlayerTypes.Player
+    game.player_type['Black'] = PlayerTypes.Player
 
     game.start()
 
@@ -81,8 +82,8 @@ def print_results(results):
         total_black_plays += result[2]
         total_black_average_time += result[3]
 
-    print("White player made, in average,", total_white_plays/len(results), " in about ", total_white_average_time/len(results), "s each!")
-    print("Black player made, in average,", total_black_plays / len(results), " in about ", total_black_average_time / len(results), "s each!")
+    print("White player made, in average, {:.6f}".format(total_white_plays / len(results)), " in about {:.6f}".format(total_white_average_time / len(results)), "s each!")
+    print("Black player made, in average, {:.6f}".format(total_black_plays / len(results)), " in about {:.6f}".format(total_black_average_time / len(results)), "s each!")
 
 def benchmark(player1_type, player2_type, n_games, board_type):
     p1_heur, p1_dept = get_heuristic_depth(player1_type)
@@ -94,8 +95,9 @@ def benchmark(player1_type, player2_type, n_games, board_type):
         results.append(result)
         count += 1
 
+    pygame.quit()
     print_results(results)
 
 if __name__ == '__main__':
-    benchmark(PlayerTypes.CpuGreedy, PlayerTypes.CpuL3, 10, BoardTypes.Board_5X5)
+    benchmark(PlayerTypes.CpuL3, PlayerTypes.CpuL2, 10, BoardTypes.Board_5X5)
  
